26544dc32b1bc24afe4ee1a03001e606bee0000d
Ravishankar <rav8291070359@gmail.com>
2022-08-18 07:14:43 +0530

    first commit

---
+++ detailspage.js
@@ -0,0 +1,41 @@
+var products = localStorage.getItem("products");
+var length=products.length
+// for (index=0; index <length; index++){
+//     document.getElementById('pag').innerHTML+=`<li class="page-item"><a class="page-link" href="#">1</a></li>
+//     <li class="page-item active">
+//       <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+//     </li>`
+    
+// }
+
+function getDetails() {
+    
+    if(products){
+    products=JSON.parse(products)
+    var body = document.getElementById("add")
+    
+    
+    for (i = 0; i < products.length; i++) {
+        body.innerHTML +=
+            `<tr><th scope='row'>${i+1}</th>
+            <td>${products[i].name}</td><td>${products[i].unit}</td>
+            <td>${products[i].price}</td><td>${products[i].desc}</td>
+            <td><button class="btn btn-primary" onclick="edit(${i})" style="border-radius:10px;">Edit</button></td>
+            <td><button class="btn btn-primary" onclick="deleteProduct(${i})" style="border-radius:10px;">Delete</button></td></tr>`
+    }
+}
+}
+function edit(num){
+    var para = new URLSearchParams();
+    para.append("id", num)
+    location.href = "index.html?" + para.toString();
+
+}
+function deleteProduct(num){
+    console.log(num)
+    // var products = JSON.parse(localStorage.getItem("products"));
+    products.splice(num,1)
+    localStorage.setItem("products",JSON.stringify(products))
+    location.reload()
+}
+
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const a = document.getElementById("submit")
 
-
-var products = []
-localStorage.setItem("products",JSON.stringify(products))
-if(a){
 a.addEventListener('click', addProduct)
-}
+
 
 function Product(name, unit, price, desc) {
 
@@ -22,6 +18,8 @@ function addProduct() {
     var unit = document.getElementById("unit").value
     var price = document.getElementById("price").value
     var desc = document.getElementById("desc").value
+    var para = new URLSearchParams(window.location.search);
+    var id = +para.get("id");
     if (product == "") {
         alert("name is missing")
     } else if (unit == "") {
@@ -31,13 +29,48 @@ function addProduct() {
     } else if (desc == "") {
         alert("desc is missing")
     } else {
-        products=JSON.parse(localStorage.getItem("products"))
-        products.push(new Product(product, unit, price, desc))
-        localStorage.setItem("products",JSON.stringify(products));
-        
+        let products = localStorage.getItem("products")
+        if (products == null) {
+            products = [new Product(product, unit, price, desc)]
+            localStorage.setItem("products", JSON.stringify(products));
+        } else {
+            products = JSON.parse(localStorage.getItem("products"))
+            if(id!=null){
+              products[id].name=document.getElementById("productname").value
+              products[id].unit=document.getElementById("unit").value
+              products[id].price=document.getElementById("price").value
+              products[id].desc=document.getElementById("desc").value
+              console.log(document.getElementById("productname").value)
+
+            }else{
+            products.push(new Product(product, unit, price, desc))
+            }
+            localStorage.setItem("products", JSON.stringify(products));
+        }
+
         window.location.href = "detailspage.html";
 
 
         console.log(products);
     }
+}
+
+function checkEdit() {
+    var para = new URLSearchParams(window.location.search);
+    var id = para.get("id");
+    console.log(id)
+    if(id){
+    let products = localStorage.getItem("products")
+    if (products != null) {
+        products = JSON.parse(localStorage.getItem("products"))
+        id=Number(id)
+        document.getElementById("productname").value=products[id].name
+        document.getElementById("unit").value=products[id].unit
+        document.getElementById("price").value=products[id].price
+        document.getElementById("desc").value=products[id].desc
+    }else{
+        alert("not able to edit")
+    }
+}
+    
 }
